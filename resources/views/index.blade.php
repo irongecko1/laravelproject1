0b46a40020561c5dab6a54f4dd9fa135e93f0207
Thomas Mckay <Thomas.Mckay@transnet.net>
2023-03-30 20:01:31 +0200

    First commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Demo Laravel Site</title>
+    <meta name="description" content=""/>
+    <meta name="keywords" content=""/>
+
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
+    <!--Replace with your tailwind.css once created-->
+
+    <style>
+        @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap");
+
+        html {
+            font-family: "Poppins", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+        }
+    </style>
+</head>
+
+<body class="m-4">
+<div class="container mx-auto px-40">
+    <h1 class="text-green-500 text-4xl font-bold">
+        Laravel Tutorials
+    </h1>
+    <strong>Tailwind CSS Tutorial</strong>
+    <p class="mt-4 text-md leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">Tailwind CSS is a utility-first CSS framework that makes building custom user interfaces fast and easy. Tailwind
+        is designed to be highly customizable, enabling developers and designers to build completely bespoke designs
+        without ever leaving the comfort of a single stylesheet. With Tailwind, you get access to a wide range of
+        utility classes that make it easy to create complex, responsive layouts with minimal code.</p>
+
+    <p class="mt-4 text-md leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">Tailwind's utility-first approach lets you focus on the content of your designs, rather than getting bogged down
+        in the details. Tailwind's classes are designed to be highly specific and composable, so you can mix and match
+        them to create custom designs without writing any custom CSS. Tailwind also makes it easy to customize the look
+        and feel of your designs with themeable variables and custom color palettes.</p>
+
+    <p class="mt-4 text-md leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">Tailwind includes all the features you'd expect from a modern CSS framework, including ready-made components,
+        responsive breakpoints, and powerful plugins. Tailwind also includes a built-in linter to help you keep your
+        code clean and maintainable. With Tailwind, you get all the benefits of a modern CSS framework without any of
+        the hassle of writing custom CSS.</p>
+
+    <p class="mt-4 text-md leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">Tailwind is a great choice for developers and designers who want to quickly create custom designs without
+        sacrificing maintainability or performance. Whether you're building a complex web application or a simple
+        landing page, Tailwind makes it easy to create beautiful, responsive designs with minimal effort.</p>
+
+</div>
+</body>
+</html>
